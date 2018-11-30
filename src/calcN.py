f14245097c80ad7bfa0db6db2c1d797089692ab9
Masahiro Itabashi <itabasi.lm@gmail.com>
2018-11-30 22:02:47 +0900

    Add calcN.py

---
+++ src/calcN.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# -*- coding: utf-8 -*-
+#
+# Last modified: Fri, 30 Nov 2018 21:57:42 +0900
+import numpy as np
+
+
+def calcN(cellDf):
+    '''
+    Calculate an estimator N(t).
+    N(t) refers the number of cells at the time.
+
+    Parameters
+    ----------
+    cellDf : pandas.core.frame.DataFrame
+             A pandas DataFrame which includes tracking result.
+             Column indices are like below
+             - ID
+             - uID
+             - motherID
+             - daughter1ID
+             - daughter2ID
+             - cenX
+             - cenY
+             - Z
+             - cellNo
+             ...
+
+    Returns
+    -------
+    N : list
+        A list of the cell numbers at each time.
+    '''
+    N = list()
+
+    for i in range(np.max(cellDf['Z'])):
+        existingCell = cellDf[cellDf['Z'] == i]
+        N.append(len(existingCell))
+
+    return N
+
+
+if __name__ == "__main__":
+    from loadSchnitz import loadSchnitz
+    matFilePath = ('/Users/itabashi/Research/Analysis/Schnitzcells/'
+                   '9999-99-99/488/data/488_lin.mat')
+    cellDf = loadSchnitz(matFilePath)
+    N = calcN(cellDf)
+    print(N)
--- src/createGraph.py
+++ src/createGraph.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8 :
 # -*- coding: utf-8 -*-
 #
-# Last modified: Fri, 30 Nov 2018 21:26:08 +0900
+# Last modified: Fri, 30 Nov 2018 21:48:49 +0900
 import networkx as nx
 
 
@@ -23,10 +23,8 @@ def createGraph(cellDf):
     graph.add_nodes_from(cellDf['uID'])
     for i in range(len(cellDf['uID'])):
         if cellDf['daughter1ID'][i] > 0:
-            # graph.add_edge(cellDf['uID'][i], cellDf['daughter1ID'][i])
             graph.add_edge(cellDf['daughter1ID'][i], cellDf['uID'][i])
         if cellDf['daughter2ID'][i] > 0:
-            # graph.add_edge(cellDf['uID'][i], cellDf['daughter2ID'][i])
             graph.add_edge(cellDf['daughter2ID'][i], cellDf['uID'][i])
 
     adjSparseMat = nx.adjacency_matrix(graph)
