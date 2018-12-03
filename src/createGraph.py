3c08d5105c9263fb1cf1cde285dedb5bf6abd33d
Masahiro Itabashi <itabasi.lm@gmail.com>
2018-12-03 18:10:57 +0900

    Use igraph instead of networkx in createGraph.py

--- src/createGraph.py
+++ src/createGraph.py
@@ -2,43 +2,46 @@
 # vim: set fileencoding=utf-8 :
 # -*- coding: utf-8 -*-
 #
-# Last modified: Fri, 30 Nov 2018 21:48:49 +0900
-import networkx as nx
+# Last modified: Mon, 03 Dec 2018 16:44:44 +0900
+import numpy as np
+import igraph
 
 
-def createGraph(cellDf):
+def createGraph(cellDf, attr=None):
     '''
     Create a graph of adjacency matrix.
 
     Parameters
     ----------
     cellDf : pandas.core.frame.DataFrame
+    attr : string
 
     Returns
     -------
-    graph : networkx.classes.digraph.DiGraph
+    graph : igraph.Graph
     adjMat : numpy.ndarray
     '''
-    graph = nx.DiGraph()
-    graph.add_nodes_from(cellDf['uID'])
-    for i in range(len(cellDf['uID'])):
-        if cellDf['daughter1ID'][i] > 0:
-            graph.add_edge(cellDf['daughter1ID'][i], cellDf['uID'][i])
-        if cellDf['daughter2ID'][i] > 0:
-            graph.add_edge(cellDf['daughter2ID'][i], cellDf['uID'][i])
+    graph = igraph.Graph()
+    graph.add_vertices(len(cellDf))
 
-    adjSparseMat = nx.adjacency_matrix(graph)
-    adjMat = adjSparseMat.toarray()
+    for i in range(len(cellDf)):
+        if cellDf['daughter1ID'][i] > 0:
+            graph.add_edge(cellDf['uID'][i], cellDf['daughter1ID'][i])
+        if cellDf['daughter2ID'][i] > 0:
+            graph.add_edge(cellDf['uID'][i], cellDf['daughter2ID'][i])
+
+    adjMat = np.array(list(graph.get_adjacency()))
+
+    if attr is not None:
+        graph[attr] = cellDf[attr]
 
     return graph, adjMat
 
 
 if __name__ == "__main__":
     from loadSchnitz import loadSchnitz
-
     matFilePath = ('/Users/itabashi/Research/Analysis/Schnitzcells/'
                    '9999-99-99/488/data/488_lin.mat')
     cellDf = loadSchnitz(matFilePath)
     graph, adjMat = createGraph(cellDf)
-    import numpy as np
     np.savetxt('cG.csv', adjMat, fmt='%d', delimiter=',')
