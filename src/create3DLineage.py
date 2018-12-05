abb4c64b064b1111306bb84dae28d728dc182939
Masahiro Itabashi <itabasi.lm@gmail.com>
2018-12-05 14:30:02 +0900

    Use igraph.Graph instead of networkx in create3DLineage.py

--- src/create3DLineage.py
+++ src/create3DLineage.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8 :
 # -*- coding: utf-8 -*-
 #
-# Last modified: Fri, 30 Nov 2018 19:42:54 +0900
+# Last modified: Wed, 05 Dec 2018 14:27:11 +0900
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -11,7 +11,7 @@ import os
 from createGraph import createGraph
 
 
-def create3DLineage(cellDfWP, dt, savePath, attribute):
+def create3DLineage(cellDfWP, dt, attr=None, savePath=None):
     '''
     Draw 3D lineage.
 
@@ -24,20 +24,23 @@ def create3DLineage(cellDfWP, dt, savePath, attribute):
     dt : numeric
          An image interval. This Parameter confluence the z axis tics
          of 3D lineage.
+    attr : string
     savePath : string
                A path in which the result images will be saved.
-    attribute : string
-                
 
     Returns
     -------
-
+    fig : matplotlib.figure.Figure
     '''
-    graph = createGraph(cellDfWP)
+    graph, adjMat = createGraph(cellDfWP, attr)
 
-    maxPhe = max(cellDfWP[attribute])
-    colors = {key: plt.cm.gnuplot(float(value)/float(maxPhe))
-              for key, value in cellDfWP[attribute].iteritems()}
+    if attr is not None:
+        maxPhe = max(cellDfWP[attr])
+        colors = {key: plt.cm.gnuplot(float(value)/float(maxPhe))
+                  for key, value in cellDfWP[attr].iteritems()}
+    else:
+        colors = {i: (0, 0, 0)
+                  for i in range(len(cellDfWP))}
 
     pos = {cellDfWP['uID'][i]:
            (cellDfWP['cenX'][i] - min(cellDfWP['cenX']),
@@ -59,11 +62,13 @@ def create3DLineage(cellDfWP, dt, savePath, attribute):
             ax.scatter(xi, yi, zi, s=1,
                        c=colors[i], alpha=0, cmap=plt.cm.gnuplot)
 
-    for i, j in enumerate(graph.edges()):
-        x = np.array((pos[j[0]][0], pos[j[1]][0]))
-        y = np.array((pos[j[0]][1], pos[j[1]][1]))
-        z = np.array((pos[j[0]][2], pos[j[1]][2]))
-        ax.plot(x, y, z, c=colors[j[1]], lw=2, alpha=1)
+    for edge in graph.es:
+        source = edge.source
+        target = edge.target
+        x = np.array((pos[source][0], pos[target][0]))
+        y = np.array((pos[source][1], pos[target][1]))
+        z = np.array((pos[source][2], pos[target][2]))
+        ax.plot(x, y, z, c=colors[target], lw=2, alpha=1)
 
     ax.set_xlabel('x', fontsize=20, labelpad=18)
     ax.set_ylabel('y', fontsize=20, labelpad=18)
@@ -72,21 +77,22 @@ def create3DLineage(cellDfWP, dt, savePath, attribute):
     ax.yaxis.set_tick_params(labelsize=12)
     ax.zaxis.set_tick_params(labelsize=12)
 
-    for angle in range(0, 360, 1):
-        ax.view_init(30, angle)
-        saveFile = os.path.join(savePath, str(angle)+'.png')
-        fig.savefig(saveFile, transparent=True)
+    if savePath is not None:
+        for angle in range(0, 360, 1):
+            ax.view_init(30, angle)
+            saveFile = os.path.join(savePath, str(angle)+'.png')
+            fig.savefig(saveFile, transparent=True)
+
+    return fig
 
 
 if __name__ == "__main__":
     from measurePhenotypes import measurePhenotypes
-
     matFilePath = ('/Users/itabashi/Research/Analysis/Schnitzcells/'
-                   '9999-99-99/488/data/488_lin.mat')
+                   '2018-08-28/488/data/488_lin.mat')
     segImgsPath = ('/Users/itabashi/Research/Analysis/Schnitzcells/'
-                   '9999-99-99/488/segmentation/')
+                   '2018-08-28/488/segmentation/')
     rawImgsPath = ('/Users/itabashi/Research/Experiment/microscope/'
                    '2018/08/28/ECTC_8/Pos0/forAnalysis/488FS/')
     cellDfWP = measurePhenotypes(matFilePath, segImgsPath, rawImgsPath)
-
     create3DLineage(cellDfWP, 10, '/Users/itabashi/Desktop/tmp3', 'area')
