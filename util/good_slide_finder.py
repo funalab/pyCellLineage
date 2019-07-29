453d748ea1beaa8c313adc609591acbacd7c14a3
Ryo Nakatani <ryo.nakatani@oist.jp>
2019-07-29 14:26:43 +0900

    Made it possible to read stacks with missing slides

--- util/good_slide_finder.py
+++ util/good_slide_finder.py
@@ -29,7 +29,6 @@ result.csv should be
  slide_num
 
 using result.csv the program copies the slides deemed good into a new directory good_slide_dir
- 
 
 '''
 
@@ -57,13 +56,15 @@ class ButtonScreen(BoxLayout):
     button_bool = True
     img_index = int()
     csv_dir = str()
+    itr = int()
 
-    def __init__(self, index, csv, sm, **kwargs):
+    def __init__(self, index, csv, sm, itr, **kwargs):
         super(ButtonScreen, self).__init__(**kwargs)
 
         self. orientation = 'horizontal'
         self.img_index = index
         self.csv_dir = csv
+        self.itr = itr
         btn1 = Button(text="OK", font='40sp', size_hint=(None, 0.3))
         btn2 = Button(text="NG", font='40sp', size_hint=(None, 0.3))
         btn1.bind(on_press=partial(self.pressed, status=0, screenmanager=sm))
@@ -74,22 +75,35 @@ class ButtonScreen(BoxLayout):
 
     def pressed(self, instance, status, screenmanager):
         good_slide_csv = self.csv_dir
-        index = self.img_index
-        if index == 0:
+        if self.img_index == 0:
             if not path.isfile(good_slide_csv):
                 print "created good_slide.csv"
             else:
                 print "Found good_slide.csv, Renewed it"
             with open(good_slide_csv, mode='w') as f:
                 f.write('slide_num,status\n')
-
+        # fill in missing slides with bad nums
+        else:
+            csv = pd.read_csv(good_slide_csv)
+            num_of_previous = csv['slide_num'].max()
+            if self.img_index - num_of_previous > self.itr:
+                print "Adding status for missing image"
+                i = num_of_previous
+                print "\tfilling in " + str(i + self.itr)
+                while i + self.itr < self.img_index:
+                    i = i + self.itr
+                    with open(good_slide_csv, mode='a') as f:
+                        img_result = str(i) + ",1\n"
+                        f.write(img_result)
         with open(good_slide_csv, mode='a') as f:
-            img_result = str(index) + "," + str(status) + "\n"
+            img_result = str(self.img_index) + "," + str(status) + "\n"
             f.write(img_result)
         self.button_bool = False
         screen_list = screenmanager.screen_names
-        if len(screen_list) != 1 and index != len(screenmanager.screen_names)-1:
-            screen_name = 'image' + str(self.img_index)
+        screen_name = 'image' + str(self.img_index)
+        missing_slides = self.img_index / self.itr - screen_list.index(screen_name)
+        max_imgs = int(screen_list[len(screen_list)-1][len('image'):])
+        if len(screen_list) != 1 and self.img_index < (max_imgs - missing_slides):
             screenmanager.current = screen_list[screen_list.index(screen_name) + 1]
         else:
             fin_screen = Screen(name="end_screen")
@@ -104,7 +118,7 @@ class MainScreen(Screen):
     csv = str()
     btn = object()
 
-    def __init__(self, idx, img, csv, sm,  **kwargs):
+    def __init__(self, idx, img, csv, sm, itr,  **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.orientation = 'vertical'
         self.padding = 50
@@ -114,7 +128,7 @@ class MainScreen(Screen):
         self.clear_widgets()
         self.img_index = idx
         self.add_widget(ImageScreen(image=img))
-        btn = ButtonScreen(index=self.img_index, csv=csv, sm=sm)
+        btn = ButtonScreen(index=self.img_index, csv=csv, sm=sm, itr=itr)
         self.btn = btn
         self.add_widget(btn)
         return
@@ -123,17 +137,20 @@ class MainScreen(Screen):
 class ImageCheckerApp(App):
     csv = str()
     img_dir = list()
+    basename = str()
+    itr = int()
 
     def build(self):
         self.title = "Good Slide Finder"
         present_path = path.abspath(".")
         target_dir = laser_dir_checker(present_path)
+        self.basename = os.path.basename(target_dir)
         image_files = sorted(os.listdir(target_dir))
         good_slide_csv = path.join(present_path, "good_slide.csv")
         new_imgs = list()
         for image in image_files:
             abs_image_dir = path.join(target_dir, image)
-            if path.isfile(abs_image_dir) and "img" in image:
+            if path.isfile(abs_image_dir):
                 new_imgs.append(abs_image_dir)
         if len(new_imgs) == 0:
             print "something went wrong can't find image "
@@ -141,10 +158,45 @@ class ImageCheckerApp(App):
         self.csv = good_slide_csv
         self.img_dir = new_imgs
         sm = ScreenManager(transition=NoTransition())
+        self.itr = self.find_iteration()
         for image in new_imgs:
-            sm.add_widget(MainScreen(sm=sm, idx=new_imgs.index(image), img=image, csv=self.csv))
-        sm.current = 'image' + str(0)
+            index = self.get_file_num(image)
+            sm.add_widget(MainScreen(sm=sm, idx=index, img=image, csv=self.csv, itr=self.itr))
+            sm.current = 'image' + str(0)
         return sm
+
+    def get_file_num(self, string):
+        string = os.path.basename(string)
+        for a in string:
+            if a == "_":
+                str_num = string.index(a) + 1
+                for b in string[str_num:]:
+                    if b == "_":
+                        end_num = string[str_num:].index(b) + str_num
+                        return int(string[str_num:end_num])
+        else:
+            print "Wrong file name %s" % string
+            exit()
+        return None
+
+    def find_iteration(self):
+        images = self.img_dir
+        org_image = os.path.basename(images[0])
+        itr_image = os.path.basename(images[1])
+        if org_image == "img_%09d_%s_000.tif" % (0, self.basename):
+            i = 0
+            for a in itr_image:
+                if a is not org_image[i]:
+                    if 10 > int(a) > 0:
+                        return int(a)
+                    else:
+                        print "file name is wrong"
+                        exit()
+                i = i+1
+        else:
+            print "Two files with the same name exist in directory"
+            exit()
+        return None
 
 
 def laser_dir_checker(present_path):
@@ -165,7 +217,7 @@ def laser_dir_checker(present_path):
 def good_itr_find(data, min_slide):
     j = 1
     i = min_slide
-    max_slide = data['slide_num'].max()
+    max_slide = len(data['slide_num'])
     while i < max_slide and j < max_slide:
         if data.iloc[i]['status'] == 0:
             i = i + j
@@ -178,7 +230,7 @@ def good_itr_find(data, min_slide):
 def good_slide_finder(good_slide_dir, dt=30, Td=100):
     good_slide = pd.read_csv(good_slide_dir)
     i = 0
-    init_len = good_slide['slide_num'].max()
+    init_len = len(good_slide['slide_num'])
     min_slide = good_slide['slide_num'].min()
     final_slides = list()
     while init_len > i:
@@ -190,7 +242,6 @@ def good_slide_finder(good_slide_dir, dt=30, Td=100):
             x = good_itr_find(good_slide, min_slide)
             slide = min_slide
             while slide < init_len:
-                print good_slide.loc[good_slide['slide_num'] == slide]
                 final_slides.append(str(good_slide.iloc[slide]['slide_num']))
                 slide = slide + x
             break
@@ -219,11 +270,16 @@ def csv_dir_finder(present_csv_dir):
     return csv_list
 
 
-def makeid_list(csv_file,basename):
+def makeid_list(csv_file, basename):
     id_list = list()
-    df = pd.read_csv(csv_file)
+    df = None
+    if os.path.isfile(csv_file):
+        df = pd.read_csv(csv_file)
+    else:
+        print "Unable to find result.csv"
+        exit()
     for i in range(len(df)):
-        id_name = "img_%09d_%s_000.tif" %( df['slide_num'][i],basename)
+        id_name = "img_%09d_%s_000.tif" % (df['slide_num'][i], basename)
         id_list.append(id_name)
     return id_list
 
@@ -236,14 +292,15 @@ def good_slide_create(present):
     dest_dir = os.path.join(present, dest_name)
     if not os.path.isdir(dest_dir):
         os.mkdir(dest_dir)
-    images = sorted(os.listdir(target_dir))
     id_list = makeid_list(csv_dirs[1], basename)
-    for image in images:
-        for id_name in id_list:
-            if id_name in image:
-                src = os.path.join(target_dir, image)
-                dest = os.path.join(dest_dir, id_name)
-                shutil.copy2(src, dest)
+    for id_name in id_list:
+        src = os.path.join(target_dir, id_name)
+        dst = os.path.join(dest_dir, id_name)
+        if os.path.isfile(src):
+            print "Found file!\nMoving %s" % src
+            shutil.copy2(src, dst)
+        else:
+            print "can't find src file %s" % src
 
 
 if __name__ == "__main__":
