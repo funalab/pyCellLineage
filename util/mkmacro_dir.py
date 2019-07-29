7788f4890e85a221aa91137fadabae310765116b
Ryo Nakatani <ryo.nakatani@oist.jp>
2019-07-29 15:19:41 +0900

    Fixed bug in mkmacro_dir.py

--- util/mkmacro_dir.py
+++ util/mkmacro_dir.py
@@ -2,7 +2,9 @@
 import os
 import shutil
 
+
 def main():
+    file_len = None
     if os.path.exists("405"):
         if not os.path.exists("macro"):
             os.mkdir("macro")
@@ -15,29 +17,38 @@ def main():
         file_num = int(raw_input("Which file would you like to make a ratio for?\n"))
         file_num_img = file_num - 1
         file_num_sntz = file_num
-        file_name_405 = '405/img_%09d_405_000.tif' %(file_num_img)
-        file_name_488 = '488/img_%09d_488_000.tif' %(file_num_img)
-        file_name_sntz = "seg_img/"+str(file_num_sntz) + ".tif"
+        file_name_405 = os.path.abspath(os.path.join("405", 'img_%09d_405_000.tif' % file_num_img))
+        file_name_488 = os.path.abspath(os.path.join("488", 'img_%09d_488_000.tif' % file_num_img))
+        file_name_sntz = os.path.abspath(os.path.join("seg_img", str(file_num_sntz) + ".tif"))
         if os.path.exists(file_name_405):
-            shutil.copy2(file_name_405,"macro/405.tif")#405
-            if not os.path.exists("macro/405.tif"):
+            des = os.path.join(os.path.abspath("macro"), "405.tif")
+            if not os.path.isdir(os.path.abspath("macro")):
+                shutil.copy2(file_name_405, des)  # 405
+            else:
                 print "Couldn't move 405!\n"
         if os.path.exists(file_name_488):
-            shutil.copy2(file_name_488,"macro/488.tif")#488
-            if not os.path.exists("macro/488.tif"):
+            des = os.path.join(os.path.abspath("macro"), "488.tif")
+            if not os.path.isdir(os.path.abspath("macro")):
+                shutil.copy2(file_name_488, des)  # 488
+            else:
                 print "Couldn't move 488!\n"
         if os.path.exists(file_name_sntz):
-            shutil.copy2(file_name_sntz,"macro/seg.tif")#seg
-            if not os.path.exists("macro/seg.tif"):
+            des = os.path.join(os.path.abspath("macro"), "seg.tif")
+            if not os.path.isdir(os.path.abspath("macro")):
+                shutil.copy2(file_name_sntz, des)  # seg
+            else:
                 print "Couldn't move segment file!\n"
-        if os.path.exists("window.roi"):
-            shutil.copy2("window.roi","macro/window.roi")#window.roi
-            if not os.path.exists("macro/window.roi"):
+        if os.path.exists(os.path.abspath("window.roi")):
+            des = os.path.join(os.path.abspath("macro"), "window.roi")
+            if not os.path.isdir(os.path.abspath("macro")):
+                shutil.copy2("window.roi", des)  # window.roi
+            else:
                 print "Couldn't move roi!\n"
         else:
             print "window.roi doesn't exist something went wrong!\n"                              
     else:
         print "Go to proper Directory!\n"
-            
+
+
 if __name__ == "__main__":
     main()
