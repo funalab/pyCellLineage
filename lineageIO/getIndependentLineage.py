691da0f463f8bc1efa3b76216858e08eee9bb1c5
Ryo Nakatani <ryo.nakatani@oist.jp>
2019-11-14 21:29:50 +0900

    Added aditional mode to get Independent changes between birth and division

--- lineageIO/getIndependentLineage.py
+++ lineageIO/getIndependentLineage.py
@@ -8,7 +8,7 @@ import pandas as pd
 import tqdm
 
 
-def getIndependentLineage(cellDfWPL):
+def getIndependentLineage(cellDfWPL,mode=None):
     '''
     Get indipendent lineage
 
@@ -26,23 +26,52 @@ def getIndependentLineage(cellDfWPL):
                   A list of lineages.
     '''
 
-    listOfLinIdx = np.unique(cellDfWPL[cellDfWPL['Z'] == np.max(cellDfWPL['Z'])]['cellNo'])
-    listOfDf = list()
-    missed = 0
-    i = 0
-    lastCellDf = cellDfWPL[cellDfWPL['Z'] == np.max(cellDfWPL['Z'])]
-    for sCell_id in lastCellDf['uID']:
-        sCellDf = cellDfWPL[cellDfWPL['uID']==int(sCell_id)]
-        if sCellDf is not None:
-            listOfDf.append(sCellDf)
-            mID = int(sCellDf['motherID'])
-            while mID >= 0:
-                sCellDf = cellDfWPL[cellDfWPL['uID'] == mID]
+    if mode is None:
+        listOfLinIdx = np.unique(cellDfWPL[cellDfWPL['Z'] == np.max(cellDfWPL['Z'])]['cellNo'])
+        listOfDf = list()
+        missed = 0
+        i = 0
+        lastCellDf = cellDfWPL[cellDfWPL['Z'] == np.max(cellDfWPL['Z'])]
+        for sCell_id in lastCellDf['uID']:
+            sCellDf = cellDfWPL[cellDfWPL['uID']==int(sCell_id)]
+            if sCellDf is not None:
+                listOfDf.append(sCellDf)
                 mID = int(sCellDf['motherID'])
-                listOfDf[i-missed] = pd.concat([sCellDf, listOfDf[i-missed]])
-        else:
-            missed = missed + 1
-        i = i+1           
+                while mID >= 0:
+                    sCellDf = cellDfWPL[cellDfWPL['uID'] == mID]
+                    mID = int(sCellDf['motherID'])
+                    listOfDf[i-missed] = pd.concat([sCellDf, listOfDf[i-missed]])
+            else:
+                missed = missed + 1
+            i = i+1
+    elif mode == "getspecific":
+        listOfLinIdx = np.unique(cellDfWPL[cellDfWPL['Z'] == np.min(cellDfWPL['Z'])]['cellNo'])
+        listOfDf = list()
+        missed = 0
+        i = 0
+        firstCellDf = cellDfWPL[cellDfWPL['Z'] == np.min(cellDfWPL['Z'])]
+        uIDs=firstCellDf['uID'].tolist()
+        for sCell_id in uIDs:
+            sCellDf = cellDfWPL[cellDfWPL['uID']==int(sCell_id)]
+            if sCellDf is not None:
+                listOfDf.append(sCellDf)
+                d2ID = int(sCellDf['daughter2ID'])
+                d1ID = int(sCellDf['daughter1ID'])
+                while d2ID <= 0:
+                    sCellDf = cellDfWPL[cellDfWPL['uID'] == d1ID]
+                    if sCellDf.empty:
+                        break
+                    d2ID = int(sCellDf['daughter2ID'])
+                    d1ID = int(sCellDf['daughter1ID'])
+                    listOfDf[i-missed] = pd.concat([sCellDf, listOfDf[i-missed]])
+                if d2ID > 0:
+                    uIDs.append(d2ID)
+                    uIDs.append(d1ID)
+            else:
+                missed = missed + 1
+            i = i+1
+
+        
     return listOfDf
 
 
