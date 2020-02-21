aa6ddddc66bdcbfc884a7fe66cb1a6e65110f27f
Ryo Nakatani <ryo.nakatani@oist.jp>
2020-02-21 14:43:58 +0900

    Made stochastic hmm prep for output files for R test

--- LIAnalysis/stochastic_hmm.py
+++ LIAnalysis/stochastic_hmm.py
@@ -16,21 +16,27 @@ from tqdm import tqdm
 import os
 
 
-def write_Class(CellDF,target):
+def write_Class(CellDF,target,save_dir):
     counter = int(0)
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
     for lineage in getIndependentLineage(CellDF):
         lin_change = list()
         time_change = list()
-        for cell_class in lineage['ATP_Class']:
+        for cell_class in lineage[target]:
             lin_change.append(cell_class)
         class_dict = {'ATP_Class':lin_change}
         df = pd.DataFrame(class_dict)
-        df.to_csv(os.path.join(target,str(counter)+".csv"))
+        df.to_csv(os.path.join(save_dir,str(counter)+".csv"))
         counter = counter + 1
 
-def hmm_prep(CellDF, save_dir, origin_frame=0):
+def hmm_prep(CellDF, save_dir=None, origin_frame=0, thr=None):
     CellDF['ATP_Class'] = pd.np.nan
-    avg_atp = sum(CellDF['ATP'])/len(CellDF['ATP'])
+    if thr == None:
+        avg_atp = sum(CellDF['ATP'])/len(CellDF['ATP'])
+    else:
+        avg_atp = thr
+        
     class_list = list()
     for uid in range(origin_frame,len(CellDF['ATP'])):
         if float(CellDF['ATP'][uid]) <= avg_atp:
@@ -38,8 +44,30 @@ def hmm_prep(CellDF, save_dir, origin_frame=0):
         else:
             class_list.append("high_atp")
     CellDF['ATP_Class'] = class_list
-    write_Class(CellDF,save_dir)
-    return
+    if save_dir != None:
+        write_Class(CellDF,'ATP_Class',save_dir)
+    return CellDF
+
+def hmm_randomize(CellDF, save_dir=None, origin_frame=0):
+    CellDF['Rand_Class'] = pd.np.nan
+    if len(CellDF['ATP_Class']) == 0:
+        print "Make ATP_Class zone first"
+        exit()
+    else:
+        lin_len = len(CellDF)
+        origin_class = list(CellDF['ATP_Class'])
+        rand_class = list(np.random.choice(origin_class,size=lin_len,replace=False))
+        CellDF['Rand_Class'] = rand_class
+    if save_dir != None:
+        save_dir = os.path.join(save_dir,"random")
+        write_Class(CellDF,'Rand_Class',save_dir)
+#        for lineage in getIndependentLineage(CellDF):
+#            lin_len = len(lineage)
+#            origin_class = lineage['ATP_Class']
+#            rand_class = list(np.random.choice(origin_class,size=lin_len,replace=False))
+#            lineage['Rand_Class'] = rand_class
+    return CellDF
+
 
 if __name__ == "__main__":
     from annotateLineageIdx import annotateLineageIdx
