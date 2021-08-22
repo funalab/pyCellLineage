90cd1f15ef3183b01f149c3bdc5ce63236ebefaf
Ryo Nakatani <ryo.nakatani@oist.jp>
2021-08-22 22:36:05 +0900

    Added All Data for Paper

---
+++ Data/GC:MS/plotGCMS.py
@@ -0,0 +1,28 @@
+"""
+Author: Joel Nakatani
+Overview:
+
+Parameters:
+"""
+
+import pyLineage as myPackage
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plotGCMS():
+    root = os.path.join(os.path.dirname(myPackage.__file__),'Data/GC:MS')
+    resultDF = pd.read_csv(os.path.join(root,'result.csv'),index_col=0)
+    resultDF = resultDF.fillna(0)
+    for chemical in resultDF.index.values:
+        plt.cla()
+        plt.clf()
+        tmp = resultDF.loc[chemical]
+        plt.bar(tmp.keys(),tmp.values)
+        plt.title(chemical)
+        plt.savefig(os.path.join(root,chemical+'.pdf'))
+
+
+if __name__ == "__main__":
+    plotGCMS()
